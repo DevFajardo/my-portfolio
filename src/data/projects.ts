1535ac5d2c57736b42dc6ce0c8c233d16c3eb601
DevFajardo <devfajardocadena@gmail.com>
2024-04-15 02:41:11 +0100

    componentization-of-projects-separation-of-project-data-and-creation-cardprojects

---
+++ src/data/projects.ts
@@ -0,0 +1,20 @@
+import memoryGame from "../components/assets/memoryGame.png";
+
+export const projects = [
+  {
+    name: "Memory Game",
+    description: "Juego de memoria",
+    image: memoryGame,
+    url: "",
+    github: "",
+    technologies: ["NextJs", "Typescript", "TailwindCss"],
+  },
+  {
+    name: "Memory Game",
+    description: "Juego de memoria",
+    image: memoryGame,
+    url: "",
+    github: "",
+    technologies: ["NextJs", "Typescript", "TailwindCss"],
+  },
+];
